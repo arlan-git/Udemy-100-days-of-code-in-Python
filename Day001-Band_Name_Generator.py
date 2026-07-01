41bcaf526d65be8c1949404a93457a4f04dbc8a1
Arlan <artechsupllc@gmail.com>
2026-07-01 19:16:03 -0400

    Fix input variable assignment syntax
    fixed input for city variable by adding a 'space and open ('

--- Day001-Band_Name_Generator.py
+++ Day001-Band_Name_Generator.py
@@ -1,8 +1,8 @@
 #1. Greeting for my program
 print("Hello there! Welcome to the A's band name generator")
 #2. Ask the user what city they grew up in
-city = input("What's the name of the city you grew up in?\n"))
+city = (input("What's the name of the city you grew up in?\n"))
 #3.Ask the user the name of their first pet
-pet_name =(input("What's your pet's name?\n"))
+pet_name = (input("What's your pet's name?\n"))
 #4. Combination of the user's city and pet name to create band name
 print("Your band name is " + city + " " +pet_name)
