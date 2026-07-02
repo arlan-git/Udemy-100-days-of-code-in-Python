52fd8d0d803ecdcf4aad3e89b04f2dd504785ea0
Arlan <toosmooth86@gmail.com>
2026-07-02 13:48:23 -0400

    Add examples of various data types in Python

---
+++ Day 002/Data Types.py
@@ -0,0 +1,21 @@
+# print(len("Hello"))
+
+#Subscripting - start with 0
+print("Hello"[-4])
+
+# 1. String
+print("123" + "345")
+print("Hello World!")
+
+# 2. Declare number as number data type (integer = whole number)
+print(123 + 345)
+
+# 2a. Large integers
+print(123_456_789)
+
+# 3. Float = Floating Point Number
+print(3.14159)
+
+# 4. Boolean either True or False
+print(True)
+print(False)
