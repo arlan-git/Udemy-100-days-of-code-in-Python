26ae9e027157c3ad2798b044fc61a093e233c56d
Arlan <toosmooth86@gmail.com>
2026-07-02 17:04:03 -0400

    Add Mathematical Operations examples in Python

---
+++ Day 002/Mathematical_Operations.py
@@ -0,0 +1,20 @@
+print("My age: " + str(12))
+
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3)
+print(6 // 3)
+print(2 ** 3)
+
+# PEMDAS Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+# ()
+# **
+# * OR /
+# + OR -
+
+# Output is 7
+print(3 * 3 + 3 / 3 - 3)
+
+# Output is 3
+print(3 * (3 + 3) / 3 - 3)
