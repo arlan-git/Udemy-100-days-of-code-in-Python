6131bcf5a78833545ca3410e2e2d8ac0b30693ff
Arlan <toosmooth86@gmail.com>
2026-07-04 09:50:27 -0400

    Implement rollercoaster height check
    Added a simple rollercoaster height check program using if-else conditions.

---
+++ Day 003/If_Else Conditions.py
@@ -0,0 +1,15 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("Yay, you can ride this ride!")
+else:
+    print("Sorry, you'll have to wait until you've grown taller, to ride this ride!")
+
+## Comparator Operators ##
+# (>) - Greater than
+# (<) - Less than
+# (>=) - Greater than or equal to
+# (<=) - Less than or equal to
+# (==) - Equal to
+# (!=) - Not equal to
