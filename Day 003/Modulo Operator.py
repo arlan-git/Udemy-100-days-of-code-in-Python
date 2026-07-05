170b0daceda37fe84e417bb4347de2b325059cb4
Arlan <toosmooth86@gmail.com>
2026-07-05 01:04:27 -0400

    Add modulo operator check for even or odd number

--- Day 003/Modulo Operator.py
+++ Day 003/Modulo Operator.py
@@ -1,1 +1,8 @@
+number_to_check = int(input("Please enter a number you would like to check: "))
 
+# If the inputted number is divisible by 2, then it is an even number
+
+if number_to_check % 2 == 0:
+    print("This is an even number!")
+else:
+    print("This is an odd number!")
