3f7cebfad5f13932752c18209a83845b771e247a
Arlan <toosmooth86@gmail.com>
2026-07-07 21:53:21 -0400

    Update states_of_america list with modifications
    Modified the second state name, accessed a state using negative index, and added new states to the list.

---
+++ Day 004/Lists.py
@@ -0,0 +1,19 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
+                     "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont",
+                     "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri",
+                     "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas",
+                     "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho",
+                     "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+#Moddify
+states_of_america[1] = "Pencilvania"
+
+#Negative indices
+states_of_america[-3]
+
+#Add items to the end
+states_of_america.append("Arland")
+
+#Extend/add to list
+states_of_america.extend(["Arland", "ANAA Universe"])
+print(states_of_america)
+
