2d94baaf0c57dca494eef47064dd264e3282a9ed
Arlan <toosmooth86@gmail.com>
2026-07-08 20:47:56 -0400

    Implement for loop to display fruit names
    Added a for loop to print fruits and their variations.

---
+++ Day 005/For Loops.py
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+print(fruits)
+
+# Loops allow us to tell the computer to repeat actions without having to write repeated code.
+# Indentation is very important in Python programming. Every time you see the : symbol used, 
+# you need to be careful about the indentation that comes afterwards.
