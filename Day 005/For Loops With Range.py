d6ee87a637473dc9e04374238cad07a16baf9562
Arlan <toosmooth86@gmail.com>
2026-07-08 21:37:16 -0400

    Add range function examples in For Loops
    Added examples of using the range function with for loops.

--- Day 005/For Loops With Range.py
+++ Day 005/For Loops With Range.py
@@ -1,1 +1,16 @@
+# Range function with For Loop
 
+# Doesn't do anything
+print(range(1, 10))
+
+# Prints 1 to 9
+for number in range(1, 10): 
+    print(number)
+
+# Prints 1 to 10
+for number in range(1, 11):  
+    print(number)
+
+# Prints 1 to 10, in increments of 3 (i.e. 1,4,7..)
+for number in range(1,11,3):
+    print(number)
