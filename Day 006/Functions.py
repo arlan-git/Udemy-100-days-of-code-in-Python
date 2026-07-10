a5423ff152d066c305e3aee769bd9f08f9191796
Arlan <toosmooth86@gmail.com>
2026-07-09 20:47:50 -0400

    Add function definition and example usage

---
+++ Day 006/Functions.py
@@ -0,0 +1,15 @@
+print("hello")
+num_char = len("hello")
+print(num_char)
+
+# to create a function
+# def my_function():
+    #do this
+    #then do this
+    #finally do this
+
+# def - defines the function when creating a new function and then give it a name
+def my_function():
+    print("hello")
+    print("goodbye")
+my_function()
