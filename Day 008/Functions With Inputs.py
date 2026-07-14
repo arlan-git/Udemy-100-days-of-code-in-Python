3510a8c212fa57abdcb8d6855a225c5162999fd0
Arlan <toosmooth86@gmail.com>
2026-07-14 16:34:07 -0400

    Add life_in_weeks function to calculate remaining weeks

--- Day 008/Functions With Inputs.py
+++ Day 008/Functions With Inputs.py
@@ -10,3 +10,10 @@ def greet_with_name(name):
     print(f"How are you {name}?")
 
 greet_with_name("Pedro")
+
+def life_in_weeks(age):
+    years_remaining = 90 - age
+    weeks_remaining = years_remaining * 52
+    print(f"You have {weeks_remaining} weeks left.")
+
+life_in_weeks(12)
