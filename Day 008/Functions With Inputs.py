39af6f08942424677d42a377e409df32017d5c31
Arlan <toosmooth86@gmail.com>
2026-07-14 16:21:13 -0400

    Add greet_with_name function for personalized greeting

--- Day 008/Functions With Inputs.py
+++ Day 008/Functions With Inputs.py
@@ -1,1 +1,12 @@
-test
+# def greet():
+    #print("World Cup 2026, Semifinals.")
+    #print("France vs Spain")
+    #print("Argentina vs England")
+
+#greet()
+
+def greet_with_name(name):
+    print(f"Hello, {name}!")
+    print(f"How are you {name}?")
+
+greet_with_name("Pedro")
